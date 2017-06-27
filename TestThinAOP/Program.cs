ebe0d8f4efdf65c7985404d60b0a53c7e2cb899c
windning <gswind123@gmail.com>
2017-06-27 20:23:11 +0800

    init commit

---
+++ TestThinAOP/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ThinAOP;
+using System.Reflection;
+
+namespace TestThinAOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalc calc2 = ProxyFactory.CreateProxy<ICalc>(typeof(Calculater));
+            calc2.Add(2, 3);
+            calc2.Divide(6, 3);
+            calc2.Divide(6, 0);
+            
+            Console.ReadKey(true);
+        }
+    }
+
+    public class LogStartAttribute : PreAspectAttribute
+    {
+        public override void Action(InvokeContext context)
+        {
+            Console.WriteLine("log start!");
+        }
+    }
+
+    public class LogEndAttribute : PostAspectAttribute
+    {
+        public override void Action(InvokeContext context)
+        {
+            Console.WriteLine("log end!");
+        }
+    }
+
+    public class LogExAttribute : ExceptionAspectAttribute
+    {
+        public override void Action(InvokeContext context)
+        {
+            Console.WriteLine("log exception!");
+        }
+    }
+
+    public interface ICalc
+    {
+        void Add(int a, int b);
+        void Divide(int a, int b);
+    }
+
+    public class Calculater : ICalc
+    {
+        [LogStart]
+        [LogEnd]
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("a+b=" + (a + b).ToString());
+        }
+
+        [LogEx]
+        public void Divide(int a, int b)
+        {
+            Console.WriteLine("a/b=" + (a / b).ToString());
+        }
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# ThinAOP  
+## NOTE: this is not the original version of the AOP frame  
+  
+The code is from blog and this is the storing-up version of myself. The original version is [HERE](http://www.cnblogs.com/wang_yb/archive/2011/08/10/2133405.html)  
